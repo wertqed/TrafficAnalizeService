5e10a5abab9d613713326efb82cd874f558baafa
Shakurov Roman Azatovich <relife@inbox.ru>
2017-12-20 02:20:14 +0400

    убрал наны

--- DataLoad.py
+++ DataLoad.py
@@ -2,16 +2,13 @@
 import numpy as np
 from pandas import read_csv
 import datetime
-import pandas
 
 
 # Загрузка из CSV файла
 def get_csv_data():
-    dataframe = read_csv('NC.csv', engine='python', nrows=1000)
-    data = dataframe.loc[(dataframe['driver_age']) > 0 & np.isnan(dataframe['driver_gender'] == False) & np.isnan(
-        dataframe['stop_time'] == False)]
-    return data[['driver_age', 'driver_gender', 'stop_time']]
-
+    dataframe = read_csv('NC.csv', engine='python', nrows=2000).dropna(subset=['driver_age', 'driver_gender', 'stop_time'])
+    data = dataframe.loc[(dataframe['driver_age']) > 0]
+    return dataframe[['driver_age', 'driver_gender', 'stop_time']]
 
 def sex_to_bool(sex):
     if sex == "M":
@@ -20,8 +17,6 @@ def sex_to_bool(sex):
 
 
 def time_to_seconds(time):
-    if pandas.isnull(time):  # Хз что делать данные не чистятся от нулов почему не понятно,мб че лучше придумаешь
-        return 0
     return datetime.timedelta(hours=float(time[0:2]), minutes=float(time[3:5])).seconds
 
 
