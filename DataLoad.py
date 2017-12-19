a317655e63261211c86a46b0263ca2251e63af3a
IBS\VBelov <student>
2017-12-19 22:49:44 +0400

    Преобразовал данные в правильный вид

--- DataLoad.py
+++ DataLoad.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as plt
 from pandas import read_csv
-import datetime, time
+from sklearn.cluster import KMeans
+import datetime
 
 
 def get_csv_data():
@@ -10,14 +10,34 @@ def get_csv_data():
     print len(dataframe)
     data = dataframe.loc[(dataframe['driver_age']) > 0 & np.isnan(dataframe['driver_gender'] == False) & np.isnan(
         ['stop_time'] == False)]
-    return data[['driver_age', 'driver_gender', 'stop_time']].as_matrix()
+    return data[['driver_age', 'driver_gender', 'stop_time']]
 
 
-print get_csv_data()
+def sex_to_bool(sex):
+    if sex == "M":
+        return 0
+    return 1
+
+
+def time_to_seconds(time):
+    return datetime.timedelta(hours=float(time[0:2]), minutes=float(time[3:5])).seconds
+
+
+def get_transformed_data():
+    data = get_csv_data()
+    data['driver_gender'] = data['driver_gender'].apply(sex_to_bool)
+    data['stop_time'] = data['stop_time'].apply(time_to_seconds)
+    return data
+
+
+print get_transformed_data()
 
 # data = self.data.loc[self.data['Number of speakers'] < 100]
 # data_for_cl = self.data[['Number of speakers', 'Latitude', 'Longitude']].as_matrix()
 # data_for_ll = self.data[['Latitude', 'Longitude']].as_matrix()
+
 # k_means = KMeans(n_clusters=8)
-# a = k_means.fit(data_for_cl)
+# a = k_means.fit(data)
 # centrx = k_means.cluster_centers_
+
+# print centrx
